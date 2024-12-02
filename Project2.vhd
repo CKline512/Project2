e96e9f4dac01d9fa90bdf07314d6a7314268b142
mel-cage <melcage1230@gmail.com>
2024-12-02 11:59:43 -0500

    Update Project2.vhd

--- Project2.vhd
+++ Project2.vhd
@@ -27,45 +27,53 @@ signal Status_Reg0 : std_logic; -- IBF (Input Buffer Full) Bit
 signal Status_Reg1 : std_logic; -- INTE (Interrupt Enable) Bit
 signal Status_Reg2 : std_logic; -- INTR (Interrupt Request) Bit
 signal X1, X2, Y1, Y2, Y3, Z1, Z2 : std_logic; 
+signal mode1_data : std_logic_vector (7 downto 0);
+signal data_reg : std_logic_vector (7 downto 0);
+signal IBF_signal, INTR_signal : std_logic;
 
 
 begin
 
-Mode_Logic : process(Status_Reg1) begin
-    if(Control_Reg0 = '0') then -- MODE 0
-        
-    elsif(Control_Reg0 = '1') then -- MODE 1  
-          
 
-
---D And P set control registers
-Assign_Registers : process(CE, A0, RD, WR) begin
-    if(CE = '0' and A0 = '0' and RD = '0' and WR = '1') then --Data in (Read Access) CE and RD asserted, A0 Deasserted
-        D <= "ZZZZZZZZ" when (not controlreg) else				
-             "ZZZZZZZZ" when (not controlreg) else
-              P when (Control_Reg0 = '0') else
-              Data_In when (Control_Reg0 = '1') else
-              "ZZZZZZZZ";
-
-        --allows P to enter D_Bus aswell as P_Reg
-        --QUALIFIED READ
-    
-    
-    elsif(CE = '0' and A0 = '1' and RD = '1' and WR = '0') then  -- Control_Reg (for write access only) CE, WR, A0 asserted
-         CR1,2 <= "ZZZZZZZZ" when (en_l = '1' and dir = '1') else
-
-						  "ZZZZZZZZ" when (en_l = '1' and dir = '0') else
-
-						  db when (en_l = '0' and dir = '1') else
-				
-						  "ZZZZZZZZ";
-    elsif(CE = '0' and A0 = '1' and RD = '1' and WR = '1') then -- Status_Reg for read access only
-        
-
+control_reg: process(WR, CE, A0, RESET) 
+    begin 
+    if (RESET = '1') then 
+        Control_Reg0 <= '0';
+        Control_Reg1 <= '0';
     end if;
-end process Assign_Registers;
+    if falling_edge(WR) then 
+        if(CE = '0' and A0 = '1')then 
+            Control_Reg0 <= D(7);
+            Control_Reg1 <= D(6);
+        end if;
+    end if; 
+end process control_reg;
 
-Control_Outputs : Process
+
+data : process(CE, WR, A0, RD, Control_Reg0) begin 
+    if(CE = '0' and RD = '0' and WR = '1') then 
+        if(A0 = '1') then 
+            if(Control_Reg0 = '1') then 
+                data_reg <= P;
+            else
+                data_reg <= mode1_data;
+            end if;
+        else 
+            data_reg(2) <= Status_Reg2;
+            data_reg(1) <= Status_Reg1;
+            data_reg(0) <= Status_Reg0;
+        end if;
+     
+    end if;
+
+end process data;
+
+mode1_register_control: process(CE, STB, A0, P)begin
+    if(CE = '0' and falling_edge(STB) and A0 = '1') then 
+        mode1_data <= P;
+    end if; 
+
+end process mode1_register_control;
 
 
 Asynch_Process : process(RD, STB, RESET) begin
@@ -86,10 +94,10 @@ Asynch_Process : process(RD, STB, RESET) begin
         -- needs to be INTR, but this is only enabled when CR = 11
         Z2 <= (Y3 and X1 and X2);
 
-        IBF <= Z1;
+        IBF_signal <= Z1;
 
         if(Control_Reg0 = '1' and Control_Reg1 = '1')then 
-            INTR <= Z2;
+            INTR_signal <= Z2;
         end if;   
     end if;
         
@@ -110,5 +118,13 @@ RESET_Process : process(RESET) begin
         Status_Reg2 <= Status_Reg2;
     end if;
 end process RESET_Process; 
-end Behavioral;
 
+Status_Reg0 <= IBF_signal;     -- IBF status
+Status_Reg1 <= Control_Reg1;  -- INTE status
+Status_Reg2 <= INTR_signal;   -- INTR status 
+
+IBF <= IBF_signal;
+INTR <= INTR_signal;
+D <= data_reg when CE = '0' else (others => 'Z');
+
+end Behavioral;
