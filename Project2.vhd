3037114f748c969b531e56f3bdc586e6c423da87
mel-cage <melcage1230@gmail.com>
2024-12-03 18:38:32 -0500

    Update Project2.vhd

--- Project2.vhd
+++ Project2.vhd
@@ -21,8 +21,8 @@ architecture Behavioral of Project2 is
 --Status_Reg.2: INTR (Interrupt Request) Bit
 
 --signal Data_In : std_logic_vector (7 downto 0);
-signal Control_Reg0 : std_logic; -- MODE bit
-signal Control_Reg1 : std_logic; -- INTE (Interrupt Enable) Bit
+signal Control_Reg : std_logic_vector(1 downto 0); --(0) = mode, (1) = INTE
+--signal Control_Reg1 : std_logic; -- INTE (Interrupt Enable) Bit
 signal Status_Reg0 : std_logic; -- IBF (Input Buffer Full) Bit
 signal Status_Reg1 : std_logic; -- INTE (Interrupt Enable) Bit
 signal Status_Reg2 : std_logic; -- INTR (Interrupt Request) Bit
@@ -35,35 +35,31 @@ signal IBF_signal, INTR_signal : std_logic;
 begin
 
 
-control_reg: process(WR, CE, A0, RESET) 
+control_reg_process: process(WR, CE, A0, RESET) 
     begin 
     if (RESET = '1') then 
-        Control_Reg0 <= '0';
-        Control_Reg1 <= '0';
+        Control_Reg <= "00";
     end if;
-    if rising_edge(WR) then 
-        if(CE = '0' and A0 = '1')then 
-            Control_Reg0 <= D(0);
-            Control_Reg1 <= D(1);
-        end if;
+    if rising_edge(WR) and CE = '0' and A0 = '1' then 
+        Control_Reg <= D(1 downto 0);
     end if; 
-end process control_reg;
+end process control_reg_process;
 
 
-data : process(CE, WR, A0, RD, Control_Reg0, P, Status_Reg2, Status_Reg1, Status_Reg0, mode1_data) begin 
-    if(CE = '0' and RD = '0' and WR = '1') then 
-        if(A0 = '1') then 
-            if(Control_Reg0 = '1') then 
-                data_reg <= P;
-            else
-                data_reg <= mode1_data;
-            end if;
-        else 
+data : process(CE, WR, A0, RD, Control_Reg, P, Status_Reg2, Status_Reg1, Status_Reg0, mode1_data) begin 
+    if(falling_edge(WR))then 
+        if(CE = '0' and RD = '0' and A0 = '1' and Control_Reg(0) = '0')then 
+            data_reg <= mode1_data;
+        end if;
+    end if;
+    
+    if(CE = '0' and RD = '0' and WR = '1' and A0 = '1' and Control_Reg(0) = '1') then 
+        data_reg <= P;
+    elsif(CE = '0' and RD = '0' and WR = '1' and A0 = '0') then
             data_reg(2) <= Status_Reg2;
             data_reg(1) <= Status_Reg1;
             data_reg(0) <= Status_Reg0;
-        end if;
-    end if;
+     end if;
     
 
 end process data;
@@ -93,13 +89,14 @@ Z2 <= (Y3 and X1 and X2);
 
 
 
-INTR_signal <= Z2 when Control_Reg0 = '1' and Control_Reg1 = '1' else '0';
+INTR_signal <= Z2 when Control_Reg = "11" else '0';
 
-IBF_signal <= Z1 when Control_Reg0 = '1' else '0';
+IBF_signal <= Z1 when Control_Reg(0) = '1' else '0';
 
 Status_Reg0 <= IBF_signal when CE = '0' else '0' ;     -- IBF status
-Status_Reg1 <= Control_Reg1 when CE = '0' else '0';  -- INTE status
+Status_Reg1 <= Control_Reg(1) when CE = '0' else '0';  -- INTE status
 Status_Reg2 <= INTR_signal when CE = '0' else '0';   -- INTR status 
+
 
 IBF <= IBF_signal;
 INTR <= INTR_signal;
